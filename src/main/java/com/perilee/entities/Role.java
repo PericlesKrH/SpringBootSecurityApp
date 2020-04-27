bc9e279fdd7f02159716d321f85ee7e1212a1f37
PericlesKrH <perrykarach@gmail.com>
2020-04-27 18:49:04 +0300

    Merge origin/master into branch_Pericles
    Conflicts:
    	pom.xml
    	src/main/java/com/example/ContainerTerminal/controllers/UserController.java
    	src/main/webapp/WEB-INF/jsp/adminTable.jsp
    	src/main/webapp/WEB-INF/jsp/c_table.jsp

--- src/main/java/com/perilee/entities/Role.java
+++ src/main/java/com/perilee/entities/Role.java
@@ -6,7 +6,12 @@
 package com.perilee.entities;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 
@@ -15,7 +20,33 @@ import javax.persistence.Entity;
  * @author perry
  */
 @Entity
+@Table(name="role")
 public class Role implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //to INDENTITY paizei me mySQL
+    private Integer rid;
+    private String rname;
+    
+
+    public Role() {
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
     
     
 }
